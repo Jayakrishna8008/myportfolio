ded2fa91a632bb1f20625cfd770a1ec834b65ace
Pujari Jayakrishna <pujarijayakrishna9160@gmail.com>
2024-09-08 15:24:05 +0530

    This is my portfolio website

---
+++ src/components/Header/index.js
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom';
+import { FaRegMoon } from "react-icons/fa";
+import './index.css'
+const Header=()=>(
+    <>
+    <nav>
+    <ul className="nav-menu">
+        <li className="nav-menu-item">
+            <Link to="/" className= 'nav-link'>
+                Home
+            </Link>
+        </li>
+        <li className="nav-menu-item">
+            <Link to="/about" className= 'nav-link'>
+                About
+            </Link>
+        </li>
+        <li className="nav-menu-item">
+            <Link to="/services" className='nav-link'>
+                Services
+            </Link>
+        </li>
+        <li className="nav-menu-item">
+            <Link to="/portfolio" className='nav-link'>
+                Portfolio
+            </Link>
+        </li>
+        <li className="nav-menu-item">
+            <Link to="/contact" className='nav-link'>
+                Contact
+            </Link>
+        </li>
+    </ul>
+</nav>
+<FaRegMoon className="darkmode-icon" />
+</>
+)
+
+export default Header
---
+++ src/components/Home/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { RiLinkedinLine } from "react-icons/ri";
+import { IoLogoInstagram } from "react-icons/io";
+import { RiTwitterLine } from "react-icons/ri";
+// import { GrCode } from "react-icons/gr";
+import "./index.css";
+import { Link } from 'react-router-dom';
+import Header from '../Header';
+
+const Home=()=>(
+      <div className='home-container'>
+        <div className='personal-information-container'>
+         <div className='portfolio'>  
+          <p className='portfolio'>Portfolio.</p>
+        </div> 
+
+        <h3>Hello, I am</h3>
+            <h1>Jayakrishna Pujari</h1>
+            <p>I am a frontend web developer, learned skills from NxtWave CCBP 4.0 Technologies.</p>
+
+            <div className='social-media'>
+            <Link to="#" className='icon'><RiLinkedinLine /></Link>
+            <Link to="#" className='icon'><IoLogoInstagram /></Link>
+            <Link to="#" className='icon'><RiTwitterLine /></Link>
+            </div>
+            <Link to="#" className='button'>Download Resume</Link>
+        </div>
+        <div className='overplay'>
+            {/* <div className='header'>
+           <Header />
+           </div> */}
+           <h1>hai</h1>
+
+        </div>
+      </div>
+)
+
+
+export default Home
